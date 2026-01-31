1a5d29a79676667870d5daed8e96042c12b7d262
Ryan Yeo <ryan_yeo@datahouse.com>
2026-02-01 00:32:53 +0800

    Fixing allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,9 +1,3 @@
-const allowedOrigins = [
-  // "https://www.yoursite.com",
-  // "http://127.0.0.1:5000",
-  "http://localhost:5173",
-  "http://192.168.1.111:5173",
-  "http://172.20.10.2:5173",
-];
+const allowedOrigins = ["https://zhan-xin.vercel.app"];
 
 export default allowedOrigins;
