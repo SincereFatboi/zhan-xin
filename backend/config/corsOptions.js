2d3d443307210d7f872ae21b613ee3604aed1f9d
Ryan Yeo <ryan_yeo@datahouse.com>
2026-02-01 01:03:04 +0800

    Still fixing cors

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -2,14 +2,12 @@ import allowedOrigins from "./allowedOrigins.js";
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
-    return callback(new Error("Not allowed by CORS"));
   },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200,
 };
 
