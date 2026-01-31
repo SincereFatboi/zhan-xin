dca112f35c6cde5728be9714ca86d13156d6f9b1
Ryan Yeo <ryan_yeo@datahouse.com>
2026-02-01 00:46:43 +0800

    Fixing CORS policy

--- backend/middlewares/credentials.js
+++ backend/middlewares/credentials.js
@@ -3,7 +3,16 @@ import allowedOrigins from "../config/allowedOrigins.js";
 export const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.status(204).json({ message: "Preflight OK" });
+    }
   }
   next();
 };
