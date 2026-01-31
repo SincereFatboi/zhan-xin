81bbc292af7afef24712ec7c52d01f8c2dcaebcf
Ryan Yeo <ryan_yeo@datahouse.com>
2026-02-01 06:09:57 +0800

    configuring domain still

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "https://zhan-xin.vercel.app",
   "http://192.168.1.111:5173",
   "http://localhost:5173",
-  "https://www.zhanxin.org/",
+  "https://www.zhanxin.org",
 ];
 
 export default allowedOrigins;
